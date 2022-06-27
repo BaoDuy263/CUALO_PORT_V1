cd278c455a75d90b1a3dadaddc99935cdc6aaa5c
trần bảo duy <duy.tranbao10@mobifone.vn>
2022-06-27 15:22:46 +0700

    pushLogout

--- src/app/dashboard-module/Component/home/home.component.ts
+++ src/app/dashboard-module/Component/home/home.component.ts
@@ -1,17 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from '../../../Service/Account/account.service'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  opened : boolean
-  constructor() {
+  opened : boolean;
+  show : boolean
+  constructor(private router: Router,private accountService : AccountService) {
     this.opened = true;
+    this.show = false;
    }
 
   ngOnInit(): void {
+      if(!localStorage.getItem('UserInfo')){
+        this.router.navigate(['/Login']);
+      }
+  }
+
+  reload(){
+    location.reload();
+  }
+
+  loadInformation() {
+    this.router.navigate(['/Home/taikhoan']);
+  }
+
+  LogOut(){
+      this.accountService.logOut();
+      this.reload()
   }
 
 }
