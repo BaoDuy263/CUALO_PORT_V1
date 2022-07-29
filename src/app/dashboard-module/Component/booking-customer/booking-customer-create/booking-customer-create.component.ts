c93beb6e3f69e3b2cb596cf7386c358d5b5613c7
DESKTOP-T8AN1S0\damva <manh.damvan10@mobifone.vn>
2022-07-29 10:53:57 +0700

    add list booking

--- src/app/dashboard-module/Component/booking-customer/booking-customer-create/booking-customer-create.component.ts
+++ src/app/dashboard-module/Component/booking-customer/booking-customer-create/booking-customer-create.component.ts
@@ -1,78 +1,122 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { BookingCustomer } from 'src/app/Model/Booking-customer';
 import { CustomerSelect } from 'src/app/Model/Customer';
 import { ProductSelect } from 'src/app/Model/Product';
 import { BookingServiceService } from 'src/app/Service/booking-customer/booking-service.service';
 
 
 @Component({
-  selector: 'app-booking-customer-create',
-  templateUrl: './booking-customer-create.component.html',
-  styleUrls: ['./booking-customer-create.component.css']
+    selector: 'app-booking-customer-create',
+    templateUrl: './booking-customer-create.component.html',
+    styleUrls: ['./booking-customer-create.component.css']
 })
 export class BookingCustomerCreateComponent implements OnInit {
+    @ViewChild('bill') bill: ElementRef<HTMLDivElement> | undefined;
+    CreateEditForm!: FormGroup
+    submited: boolean = false;
+    bookCutomerId: number = 0;
+    isCreate: boolean = true;
+    lstCustomer: CustomerSelect[] = [];
+    lsttypeMerchandise: ProductSelect[] = [];
+    bookingCustomer: BookingCustomer | undefined;
 
 
-  CreateEditForm!: FormGroup
-  submited: boolean = false;
-  bookCutomerId : number = 0;
-  isCreate : boolean = true;
-  lstCustomer : CustomerSelect[] = [];
-  lsttypeMerchandise : ProductSelect[] = [];
+    lstContainer = [
+        { "id": "20Dc", name: "Container Khô 20 feet (20DC)" },
+        { "id": "20Ot", name: "Container Khô 20 feet hở nóc (20Ot)" },
+        { "id": "40Dc", name: "Container Khô 40 feet (20DC)" },
+        { "id": "40Ot", name: "Container Khô 40 feet hở nóc(20DC)" }
+    ]
 
+    bookingTypes = [
+        { id: 1, name: "Xuất nhập bãi", value: TypeBooking.ImportAndExport },
+        { id: 2, name: "Đóng rút hàng", value: TypeBooking.PackingAndWithraw },
+    ]
 
-  lstContainer = [
-    {"id" : "20Dc" , name : "Container Khô 20 feet (20DC)"},
-    {"id" : "20Ot" , name : "Container Khô 20 feet hở nóc (20Ot)"},
-    {"id" : "40Dc" , name : "Container Khô 40 feet (20DC)"},
-    {"id" : "40Ot" , name : "Container Khô 40 feet hở nóc(20DC)"}
-  ]
-
-  constructor(private bookingService : BookingServiceService,public dialogRef: MatDialogRef<BookingCustomerCreateComponent> ) {
-    this.CreateEditForm = new FormGroup({
-      customerId: new FormControl('', Validators.required),
-      typeContainer: new FormControl('', Validators.required),
-      phoneNumber: new FormControl('',Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")),
-      typeMerchandise: new FormControl('', Validators.required),
-      noVehicle: new FormControl(),
-      weight: new FormControl(),
-      shipName: new FormControl(),
-      dateCheckOut : new FormControl(),
-    })
-  }
-
-  get customerId() { return this.CreateEditForm.get('customerId'); }
-  get typeContainer() { return this.CreateEditForm.get('typeContainer')}
-  get phoneNumber() {return this.CreateEditForm.get('phoneNumber')}
-  get typeMerchandise() {return this.CreateEditForm.get('typeMerchandise')}
-  ngOnInit(): void {
-      this.bookingService.Detail(this.bookCutomerId).subscribe(response => {
+    constructor(private bookingService: BookingServiceService, public dialogRef: MatDialogRef<BookingCustomerCreateComponent>, private renderer: Renderer2) {
         this.CreateEditForm = new FormGroup({
-          id: new FormControl(response.id),
-          customerId: new FormControl(response.customerId, Validators.required),
-          typeContainer: new FormControl(response.typeContainer, Validators.required),
-          phoneNumber: new FormControl(response.phoneNumber,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")),
-          typeMerchandise: new FormControl(response.typeMerchandise, Validators.required),
-          noVehicle: new FormControl(response.noVehicle),
-          weight: new FormControl(response.weight),
-          shipName: new FormControl(response.shipName),
-          dateCheckOut : new FormControl(response.dateCheckOut),
+            customerId: new FormControl('', Validators.required),
+            typeContainer: new FormControl('', Validators.required),
+            phoneNumber: new FormControl('', Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")),
+            typeMerchandise: new FormControl('', Validators.required),
+            noVehicle: new FormControl(),
+            weight: new FormControl(),
+            shipName: new FormControl(),
+            dateCheckOut: new FormControl(),
+            typeBooking: new FormControl('', Validators.required)
         })
-      })
-  }
+    }
 
-  onSubmit() {
-    this.submited = true;
-    if(this.CreateEditForm.valid && this.isCreate === true){
-      this.bookingService.Insert(this.CreateEditForm.value).subscribe(response => {
-          this.dialogRef.close(response);
-      });
+    get customerId() { return this.CreateEditForm.get('customerId'); }
+    get typeContainer() { return this.CreateEditForm.get('typeContainer') }
+    get phoneNumber() { return this.CreateEditForm.get('phoneNumber') }
+    get typeMerchandise() { return this.CreateEditForm.get('typeMerchandise') }
+    ngOnInit(): void {
+        this.bookingService.Detail(this.bookCutomerId).subscribe(response => {
+            this.CreateEditForm = new FormGroup({
+                id: new FormControl(response.id),
+                customerId: new FormControl(response.customerId, Validators.required),
+                typeContainer: new FormControl(response.typeContainer, Validators.required),
+                phoneNumber: new FormControl(response.phoneNumber, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")),
+                typeMerchandise: new FormControl(response.typeMerchandise, Validators.required),
+                noVehicle: new FormControl(response.noVehicle),
+                weight: new FormControl(response.weight),
+                shipName: new FormControl(response.shipName),
+                dateCheckOut: new FormControl(response.dateCheckOut),
+                typeBooking: new FormControl(response.typeBooking, Validators.required)
+            });
+            this.bookingCustomer = response
+        })
     }
-    if(this.CreateEditForm.valid && this.isCreate === false){
-      this.bookingService.Update(this.CreateEditForm.value).subscribe(response => {
-          this.dialogRef.close(response);
-      })
+
+    onSubmit() {
+        this.submited = true;
+        if (this.CreateEditForm.valid && this.isCreate === true) {
+            this.bookingService.Insert(this.CreateEditForm.value).subscribe(response => {
+                this.dialogRef.close(response);
+            });
+        }
+        if (this.CreateEditForm.valid && this.isCreate === false) {
+            this.bookingService.Update(this.CreateEditForm.value).subscribe(response => {
+                this.dialogRef.close(response);
+            })
+        }
     }
-  }
+
+    getNameCustomer(id: any): string {
+        const customerId = parseInt(id);
+        const index = this.lstCustomer.findIndex(item => item.id === customerId);
+        return this.lstCustomer[index].name
+    }
+
+    getTypeBooking(id: number): string {
+        switch (id) {
+            case TypeBooking.ImportAndExport:
+                return "Xuất nhập container"
+            case TypeBooking.PackingAndWithraw:
+                return "Đóng rút hàng"
+            default:
+                return ""
+        }
+    }
+
+    print() {
+        var divContents = this.bill?.nativeElement.innerHTML || "";
+        var printWindow = window.open('', '', "width=" + screen.availWidth + ",height=" + screen.availHeight);
+        if (printWindow) {
+            printWindow.document.write('<html><head><title>PHIẾU IN</title>');
+            printWindow.document.write('</head><body >');
+            printWindow.document.write(divContents);
+            printWindow.document.write('</body></html>');
+            printWindow.document.close();
+            printWindow.print();
+        }
+    }
+}
+
+enum TypeBooking {
+    ImportAndExport = 1,
+    PackingAndWithraw = 2,
 }
--- src/app/dashboard-module/Component/booking-customer/booking-customer-index/booking-customer-index.component.ts
+++ src/app/dashboard-module/Component/booking-customer/booking-customer-index/booking-customer-index.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as XLSX from 'xlsx';
 import { Pagination } from '../../../../Model/Table';
-import {  lstBookingCustomer } from '../../../../Model/Booking-customer'
+import { lstBookingCustomer } from '../../../../Model/Booking-customer'
 import { BookingServiceService } from 'src/app/Service/booking-customer/booking-service.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ToastrcustomService } from 'src/app/Interceptor/toastrcustom';
@@ -8,158 +9,183 @@ import { CustomerService } from 'src/app/Service/Customer/customer.service';
 import { ProductService } from 'src/app/Service/Product/product.service';
 import { BookingCustomerCreateComponent } from '../booking-customer-create/booking-customer-create.component'
 import { BookingCustomerDeleteComponent } from '../booking-customer-delete/booking-customer-delete.component'
+import { HttpClient } from '@angular/common/http';
 @Component({
-  selector: 'app-booking-customer-index',
-  templateUrl: './booking-customer-index.component.html',
-  styleUrls: ['./booking-customer-index.component.css']
+    selector: 'app-booking-customer-index',
+    templateUrl: './booking-customer-index.component.html',
+    styleUrls: ['./booking-customer-index.component.css']
 })
 export class BookingCustomerIndexComponent implements OnInit {
+    isCreate: boolean = true;
+    bookCutomerId: number = 0;
+    loadding: boolean = false;
 
-  isCreate: boolean = true;
-  bookCutomerId: number = 0;
-  loadding: boolean = false;
+    Pagination: Pagination = {
+        currentPage: 0,
+        pageSize: 0,
+        totalRecord: 0,
+        totalPage: 0,
+    }
 
-  Pagination: Pagination = {
-    currentPage: 0,
-    pageSize: 0,
-    totalRecord: 0,
-    totalPage: 0,
-  }
+    lstdata: lstBookingCustomer = {
+        currentPage: 0,
+        pageSize: 0,
+        totalRecord: 0,
+        totalPage: 0,
+        data: []
+    };
 
-  lstdata: lstBookingCustomer = {
-    currentPage: 0,
-    pageSize: 0,
-    totalRecord: 0,
-    totalPage: 0,
-    data: []
-  };
+    lstCustomer: any = [];
+    lstProduct: any = [];
 
-  lstCustomer: any = [];
-  lstProduct: any = [];
-
-  PageInfo = {
-    page: 1,
-    Keyword: '',
-    pageSize: 10
-  }
-
-
-  constructor(private bookingServiceService: BookingServiceService, public dialog: MatDialog, private toastr: ToastrcustomService,private customerService: CustomerService,private productService: ProductService) { }
-
-   ngOnInit(): void  {
-     this.Pagingdata(this.PageInfo);
-     this.GetListSelectCustomer();
-     this.GetListSelectProduct();
+    PageInfo = {
+        page: 1,
+        Keyword: '',
+        pageSize: 10
     }
 
 
-    getCustomerName(id: string ) {
-      if(this.lstCustomer){
-        let customer =   this.lstCustomer.find((x :any) => x.id == id);
-        return customer ? customer.name : "";
-      }
-    }
+    constructor(private bookingServiceService: BookingServiceService, public dialog: MatDialog,
+        private toastr: ToastrcustomService, private customerService: CustomerService, private productService: ProductService, private http: HttpClient) { }
 
-    getProductName(id: string ) {
-      if(this.lstProduct){
-        let product =   this.lstProduct.find((x :any) => x.id == id);
-        return product ? product.name : "";
-      }
+    ngOnInit(): void {
+        this.Pagingdata(this.PageInfo);
+        this.GetListSelectCustomer();
+        this.GetListSelectProduct();
     }
 
 
-  Pagingdata(PageInfo: any) {
-    this.loadding = true;
-     this.bookingServiceService.Paging(this.PageInfo.page, this.PageInfo.Keyword, this.PageInfo.pageSize).subscribe(data => {
-      this.loadding = false;
-      this.lstdata = data;
-      this.Pagination.currentPage = data.currentPage,
-        this.Pagination.pageSize = data.pageSize,
-        this.Pagination.totalPage = data.totalPage,
-        this.Pagination.totalRecord = data.totalRecord
-    })
-  }
-
-  GetListSelectCustomer() {
-    this.customerService.GetSelectList().subscribe(data => {
-      this.lstCustomer = data.data;
-    })
-  }
-
-  GetListSelectProduct() {
-    this.productService.GetSelectList().subscribe(data => {
-      this.lstProduct = data.data;
-    })
-  }
-
-
-  onSearch(e: any) {
-    this.PageInfo.Keyword = e;
-    this.PageInfo.page = 1;
-    this.Pagingdata(this.PageInfo);
-  }
-
-  openCreate() {
-    const dialogRef = this.dialog.open(BookingCustomerCreateComponent);
-    dialogRef.componentInstance.lstCustomer = this.lstCustomer;
-    dialogRef.componentInstance.lsttypeMerchandise = this.lstProduct;
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        if (result.statusCode === 200) {
-          this.toastr.showSuccess(result.message);
-          this.Pagingdata(this.PageInfo);
+    getCustomerName(id: string) {
+        if (this.lstCustomer) {
+            let customer = this.lstCustomer.find((x: any) => x.id == id);
+            return customer ? customer.name : "";
         }
-        else {
-          this.toastr.showError(result.message);
+    }
+
+    getProductName(id: string) {
+        if (this.lstProduct) {
+            let product = this.lstProduct.find((x: any) => x.id == id);
+            return product ? product.name : "";
         }
-      }
-    });
-  }
+    }
 
-  openEdit(id: number) {
-    this.isCreate = false;
-    this.bookCutomerId = id;
-    const dialogRef = this.dialog.open(BookingCustomerCreateComponent, { width: '50%' });
-    dialogRef.componentInstance.bookCutomerId = this.bookCutomerId;
-    dialogRef.componentInstance.isCreate = this.isCreate;
-    dialogRef.componentInstance.lstCustomer = this.lstCustomer;
-    dialogRef.componentInstance.lsttypeMerchandise = this.lstProduct;
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        if (result.statusCode === 200) {
-          this.toastr.showSuccess(result.message);
-          this.Pagingdata(this.PageInfo);
+
+    Pagingdata(PageInfo: any) {
+        this.loadding = true;
+        this.bookingServiceService.Paging(this.PageInfo.page, this.PageInfo.Keyword, this.PageInfo.pageSize).subscribe(data => {
+            this.loadding = false;
+            this.lstdata = data;
+            this.Pagination.currentPage = data.currentPage,
+                this.Pagination.pageSize = data.pageSize,
+                this.Pagination.totalPage = data.totalPage,
+                this.Pagination.totalRecord = data.totalRecord
+        })
+    }
+
+    GetListSelectCustomer() {
+        this.customerService.GetSelectList().subscribe(data => {
+            this.lstCustomer = data.data;
+        })
+    }
+
+    GetListSelectProduct() {
+        this.productService.GetSelectList().subscribe(data => {
+            this.lstProduct = data.data;
+        })
+    }
+
+
+    onSearch(e: any) {
+        this.PageInfo.Keyword = e;
+        this.PageInfo.page = 1;
+        this.Pagingdata(this.PageInfo);
+    }
+
+    openCreate() {
+        const dialogRef = this.dialog.open(BookingCustomerCreateComponent);
+        dialogRef.componentInstance.lstCustomer = this.lstCustomer;
+        dialogRef.componentInstance.lsttypeMerchandise = this.lstProduct;
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                if (result.statusCode === 200) {
+                    this.toastr.showSuccess(result.message);
+                    this.Pagingdata(this.PageInfo);
+                }
+                else {
+                    this.toastr.showError(result.message);
+                }
+            }
+        });
+    }
+
+    openEdit(id: number) {
+        this.isCreate = false;
+        this.bookCutomerId = id;
+        const dialogRef = this.dialog.open(BookingCustomerCreateComponent, { width: '50%' });
+        dialogRef.componentInstance.bookCutomerId = this.bookCutomerId;
+        dialogRef.componentInstance.isCreate = this.isCreate;
+        dialogRef.componentInstance.lstCustomer = this.lstCustomer;
+        dialogRef.componentInstance.lsttypeMerchandise = this.lstProduct;
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                if (result.statusCode === 200) {
+                    this.toastr.showSuccess(result.message);
+                    this.Pagingdata(this.PageInfo);
+                }
+                else {
+                    this.toastr.showError(result.message);
+                }
+            }
+        })
+    }
+
+
+    openDelete(id: number) {
+        const dialogRef = this.dialog.open(BookingCustomerDeleteComponent);
+        dialogRef.componentInstance.bookCutomerId = id;
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                if (result.statusCode === 200) {
+                    this.toastr.showSuccess(result.message);
+                    this.Pagingdata(this.PageInfo);
+
+                }
+                else {
+                    this.toastr.showError(result.message);
+                }
+            }
+        });
+    }
+
+    onChangePage(pageOfItems: any) {
+        pageOfItems.Keyword = this.PageInfo.Keyword;
+        this.PageInfo = pageOfItems
+        this.Pagingdata(pageOfItems)
+    }
+
+    fileUpload(event: any) {
+        const selectedFile = event.target.files[0];
+        // try {
+        //   this.bookingServiceService.UploadExcel(selectedFile).subscribe(res => console.log(res,'res'));
+        // } catch (error) {
+        //   this.loadding = false;
+        //   console.log(error)
+        // }
+        const fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile)
+        fileReader.onload = (event) => {
+          let binaryData = event.target?.result;
+          let workbook = XLSX.read(binaryData, { type: 'binary' });
+          workbook.SheetNames.forEach((sheet) => {
+            const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]) as any;
+            try {
+                  this.loadding = true;
+                  this.bookingServiceService.CreateBookings(JSON.stringify(data)).subscribe(response => this.Pagingdata(this.PageInfo))
+                } catch (error) {
+                  this.loadding = false;
+                  throw new Error("error 500");
+                }
+            })
         }
-        else {
-          this.toastr.showError(result.message);
-        }
-      }
-    })
-  }
-
-
-  openDelete(id: number) {
-    const dialogRef = this.dialog.open(BookingCustomerDeleteComponent);
-    dialogRef.componentInstance.bookCutomerId = id;
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        if (result.statusCode === 200) {
-          this.toastr.showSuccess(result.message);
-          this.Pagingdata(this.PageInfo);
-
-        }
-        else {
-          this.toastr.showError(result.message);
-        }
-      }
-    });
-  }
-
-  onChangePage(pageOfItems: any) {
-    pageOfItems.Keyword = this.PageInfo.Keyword;
-    this.PageInfo = pageOfItems
-    this.Pagingdata(pageOfItems)
-  }
-
-
+    }
 }
