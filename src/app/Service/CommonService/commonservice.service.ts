f316048f6cf633608732f8979944bfd90f075d6c
DESKTOP-1K5521S\damva <damvanmanh441995@gmail.com>
2022-08-15 09:05:59 +0700

    fix common service

--- src/app/Service/CommonService/commonservice.service.ts
+++ src/app/Service/CommonService/commonservice.service.ts
@@ -6,9 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CommonserviceService {
-  //_urlApi: string = 'https://45.124.94.191:5001/api/';
+  _urlApi: string = 'https://45.124.94.191:5001/api/';
   //  _urlApi: string = 'https://localhost:5001/api/';
-  _urlApi: string = 'http://localhost:7611/api/';
+  // _urlApi: string = 'http://localhost:7611/api/';
   constructor(
     private http: HttpClient
   ) {}
