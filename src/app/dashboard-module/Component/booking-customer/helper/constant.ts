b323262668092885ac30c8ae6635be3cba30283c
DESKTOP-1K5521S\damva <damvanmanh441995@gmail.com>
2022-08-18 10:01:07 +0700

    edit create container

---
+++ src/app/dashboard-module/Component/booking-customer/helper/constant.ts
@@ -0,0 +1,33 @@
+export const activitiesPacking = [
+  { value: 2, name: "Cấp rỗng" },
+  { value: 9, name: "Đóng rời" }
+]
+
+export const activitiesData = [
+  { value: 0, name: "Chưa xác định" },
+  { value: 1, name: "Hạ bãi chờ xuất" },
+  { value: 2, name: "Cấp rỗng" },
+  { value: 3, name: "Trả nguyên" },
+  { value: 4, name: "ship side" },
+  { value: 5, name: "Lấy nguyên" },
+  { value: 6, name: "Rút ruột" },
+  { value: 7, name: "Trả bãi" },
+  { value: 8, name: "Kẹp chì" },
+  { value: 9, name: "Đóng rời" },
+]
+
+export const lstStatusData = [
+  { value: 1, name: "Vận chuyển" },
+  { value: 2, name: "Tại cảng" },
+  { value: 3, name: "Ngoài cảng" },
+]
+
+export const lstTypeDelivery = [
+  { value: 1, name: "Tau bai" },
+  { value: 2, name: "bai xe" },
+  { value: 3, name: "bai xe sl" },
+]
+
+export const lstTypeContData = [
+  "container20f", "container40f"
+]
