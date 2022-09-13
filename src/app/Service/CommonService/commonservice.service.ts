0f847746fb2ad3cad88780e7bf6157a8e3f98e84
trần bảo duy <duy.tranbao10@mobifone.vn>
2022-09-13 14:48:27 +0700

    pushtumlum

--- src/app/Service/CommonService/commonservice.service.ts
+++ src/app/Service/CommonService/commonservice.service.ts
@@ -11,7 +11,7 @@ export class CommonserviceService {
   //_urlApi: string = environment.urlApi + '/api/';
   //  _urlApi: string = 'http://localhost:5000/api/';
   _urlApi: string = 'http://localhost:7611/api/';
-
+  
   constructor(
     private http: HttpClient
   ) {}
